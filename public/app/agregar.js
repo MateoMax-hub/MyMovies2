436aa854c0ac0760c3c3f0507ab124f1540be53c
andrescuello7 <andrescuellotrabajo@gmail.com>
2020-11-24 18:00:30 -0300

    index v-1

--- public/app/agregar.js
+++ public/app/agregar.js
@@ -35,21 +35,39 @@ function displayUser() {
     for (let i = 0; i < userUI.length; i++) {
         const agregar = userUI[i];
         const tr =
-        `<div class="card" style="width: 14rem; margin: 2%;">
-            <button type="button" onclick="deleteUser('${agregar.id}')" class="close">
-                <span aria-hidden="true">&times;</span>
-            </button> 
-            <img src="https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg"
-                class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${agregar.titulo}</h5>
-                <p class="card-text"><b>Descripcion:</b>${agregar.descripcion}</p>
-                <div style="display: flex; justify-content: space-between;">
-                    <a href="#" class="btn btn-dark">Alquilar</a>
-                    <a href="#" class="btn btn-success">Ver</a>
-                </div>
-            </div>
-        </div>`
+        `<div style="margin: 0px 3%;">
+                    <!-- Button trigger modal -->
+                    <div class="CardsIndividuales">
+                        <div class="card" style="margin: 0%;">
+                            <button type="button" onclick="deleteUser('${agregar.id}')" class="close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <a type="button" data-toggle="modal" data-target="#exampleModal1">
+                            <img src="https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg"
+                                class="card-img-top" alt="...">
+                        </a>
+                    </div>
+    
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel"
+                        aria-hidden="true">
+                        <div class=" modal-dialog">
+                            <div class="modal-content">
+                                <div class="CardsModal card-body">
+                                    <h5 class="card-title">${agregar.titulo}</h5>
+                                    <br>
+                                    <p class="card-text"><b>Descripcion:</b>${agregar.descripcion}</p>
+                                </div>
+                                <div class="CardsModal modal-body">
+                                    <div style="display: flex; justify-content: space-between;">
+                                        <a href="#" class="btn btn-success">Alquilar</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
         row.push(tr);
     }
     subirCard.innerHTML = row.join('');
@@ -65,3 +83,19 @@ function deleteUser(userUIid) {
     localStorage.setItem('userUI', userAgregarJson);
     displayUser()
 }
+
+//  <div class="card" style="width: 14rem; margin: 2%;">
+//             <button type="button" onclick="deleteUser('${agregar.id}')" class="close">
+//                 <span aria-hidden="true">&times;</span>
+//             </button> 
+//             <img src="https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg"
+//                 class="card-img-top" alt="...">
+//             <div class="card-body">
+//                 <h5 class="card-title">${agregar.titulo}</h5>
+//                 <p class="card-text"><b>Descripcion:</b>${agregar.descripcion}</p>
+//                 <div style="display: flex; justify-content: space-between;">
+//                     <a href="#" class="btn btn-dark">Alquilar</a>
+//                     <a href="#" class="btn btn-success">Ver</a>
+//                 </div>
+//             </div>
+//         </div>
