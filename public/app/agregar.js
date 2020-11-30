552bc6debca6700da339afb3c69622dfc3fe93e4
Mateo-Max <max.mateo.02@gmail.com>
2020-11-30 15:46:23 -0300

    filter agregado, comedia

--- public/app/agregar.js
+++ public/app/agregar.js
@@ -56,10 +56,12 @@ formularioAgregar.onsubmit = (e) => {
 
 function displayUser() {
     const userUI = JSON.parse(localStorage.getItem('userUI')) || [];
-    const row = [];
+    const rowAgregados = [];
+    const rowComedia = [];
     console.log("funciona pelicula", userUI);
-    for (let i = 0; i < userUI.length; i++) {
-        const agregar = userUI[i];
+    const nuevosAgregados = userUI.filter((agregar) => agregar.categoria === 'Nuevos Agregados')
+    for (let i = 0; i < nuevosAgregados.length; i++) {
+        const agregar = nuevosAgregados[i];
         const tr =
             `<div class="prueba">
                     <!-- Button trigger modal -->
@@ -139,8 +141,95 @@ function displayUser() {
                     </div>
                 </section>`
 
-        row.push(tr);
+        rowAgregados.push(tr);
     }
+    const comedia = userUI.filter((agregar) => agregar.categoria === 'Comedia')
+    console.log("🚀 ~ file: agregar.js ~ line 146 ~ displayUser ~ comedia", comedia)
+    for (let i = 0; i < comedia.length; i++) {
+        const agregar = comedia[i];
+        const tr =
+            `<div class="prueba">
+                    <!-- Button trigger modal -->
+                    <div class="CardsIndividuales">
+                        <a type="button" data-toggle="modal" data-target="#exampleModal${[i]}">
+                            <img src="${agregar.imagen}"
+                            class="card-img-top" alt="...">
+                        </a>
+                    </div>
+    
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal${[i]}" tabindex="-1" aria-labelledby="exampleModalLabel"
+                        aria-hidden="true">
+                        <div class=" modal-dialog">
+                            <div class="modal-content">
+                                <div class="CardsModal card-body">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                    <h5 class="card-title">${agregar.titulo}</h5>
+                                    <br>
+                                    <img src="${agregar.imagen}"class="card-img-top" alt="...">
+                                    <br>
+                                    <br>
+                                    <p class="card-text"><b>Descripcion: </b>${agregar.descripcion}</p>
+                                    <p class="card-text"><b>Precio: </b>$${agregar.precio}</p>
+                                </div>
+                                <div class="CardsModal modal-body">
+                                    <div style="display: flex; justify-content: space-between;">
+                                        <!-- Button trigger modal -->  
+                                        <a href="#" class="btn btn-success">Alquilar</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Seccion de Cambios de Pelicula -->
+                <section>
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModalUi" tabindex="-1" aria-labelledby="exampleModalLabel"
+                        aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Cambios</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="modal-body">
+                                        <form id="CambiarPeli">
+                                            <div class="input-group" style="display: flex; flex-direction: column;">
+                                                <div style="display: flex;">
+                                                    <input type="text" id="TituloPeliTwo" class="form-control w-100"
+                                                        aria-label="Text input with checkbox" placeholder="Titulo">
+                                                </div>
+                                            </div>
+                                            <br>
+                                            <div class="input-group" style="display: flex; flex-direction: column;">
+                                                <div style="display: flex;">
+                                                    <input type="text" id="DescripcionPeliTwo" class="form-control w-100"
+                                                        aria-label="Text input with checkbox"
+                                                        placeholder="Descripcion de pelicula...">
+                                                </div>
+                                            </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                    <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>`
+
+        rowComedia.push(tr);
+    }
+
+
     // if (userUI.filter((agregar) => agregar.categoria === 'Comedia')) {
     //     comediaUI.innerHTML = row.join('');
     // } if (userUI.filter((agregar) => agregar.categoria === 'Terror')) {
@@ -151,7 +240,8 @@ function displayUser() {
     //     subirCard.innerHTML = row.join('');
     // }
 
-    subirCard.innerHTML = row.join('');
+    subirCard.innerHTML = rowAgregados.join('');
+    comediaUI.innerHTML = rowComedia.join('')
 }
 displayUser()
 
