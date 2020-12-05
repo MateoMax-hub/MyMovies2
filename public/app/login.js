9e5965fd87f7467605eaa0ea3154e86dbb5981dc
Mateo-Max <max.mateo.02@gmail.com>
2020-12-05 17:46:38 -0300

    usuario admin default y bug con el session encontrado por jj solucionado

--- public/app/login.js
+++ public/app/login.js
@@ -2,6 +2,29 @@ const loginForm = document.getElementById('loginForm');
 const userLogin = document.getElementById('userLoginInput');
 const passwordLogin = document.getElementById('passwordLoginInput');
 
+const usersRegister= JSON.parse(localStorage.getItem('usersRegister')) || [];
+const userAdminDefault = {usuario: "pelisAdmin", email: "hi@hi", password: "123admin", id: "_111111111", admin: true}
+usersRegister.push(userAdminDefault)
+
+const local1 = localStorage.key(0)
+const local2 =localStorage.key(1)
+const local3 =localStorage.key(2)
+const arrayLocal = [{key: local1},{key: local2},{key: local3}]
+
+const userSesionFind = arrayLocal.find ((k) => k.key == 'sesion')
+if (userSesionFind == undefined) {
+    const falseSession = JSON.stringify('falseSession')
+    localStorage.setItem('sesion', falseSession)
+}
+const userRegisterFind = arrayLocal.find ((k) => k.key == 'usersRegister')
+if (userRegisterFind == undefined) {
+    console.log('hi');
+    const userAdminDefaulJSON = JSON.stringify(usersRegister)
+    localStorage.setItem('usersRegister',userAdminDefaulJSON)
+} else {
+}
+
+
 if (JSON.parse(localStorage.getItem('sesion')) == 'falseSession') {
 } else {
     window.location.href= ('./index.html')
--- public/app/perfil.js
+++ public/app/perfil.js
@@ -1,14 +1,12 @@
 const YouName= document.getElementById('suName');
 
 function displayUsers(){
-    const usersRegister= JSON.parse(localStorage.getItem('usersRegister')) || [];
+    const usersRegister= JSON.parse(localStorage.getItem('sesion')) || [];
     const rows= [];
-    for (let i = 0; i < usersRegister.length; i++) {
-        const user = usersRegister[i];
-        const tr=
-        `<h5 class="NombreUsers">${user.usuario}</h5>`
-        rows.push(tr)
-    }
+    const tr=
+    `<h5 class="NombreUsers">${usersRegister.usuario}</h5>`
+    rows.push(tr)
+    
     YouName.innerHTML= rows.join('')
 }
 displayUsers()
