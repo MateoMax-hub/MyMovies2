4184501002efccd22c0315c6459bae608d64aaaa
andrescuello7 <andrescuellotrabajo@gmail.com>
2020-12-16 21:02:43 -0300

    datos prueba terminado

--- public/app/datos.js
+++ public/app/datos.js
@@ -1,6 +1,20 @@
-fetch('../../src/assets/json/usersPrueba.json')
-    .then(response => response.json())
-    .then(data => {
-  	// Do something with your data
-    console.log(data);
-});
+function addDatosTest() {
+  fetch('../../src/assets/json/usersPrueba.json')
+      .then(response => response.json())
+      .then(data => {
+      // Do something with your data
+      const dataJSON = JSON.stringify(data)
+      localStorage.setItem('usersRegister',dataJSON)
+  });
+  fetch('../../src/assets/json/pelisPrueba.json')
+      .then(response => response.json())
+      .then(data => {
+      // Do something with your data
+      const dataJSON = JSON.stringify(data)
+      localStorage.setItem('userUI',dataJSON)
+  });
+
+}
+addDatosTest()
+
+
