aaba455d56cf6c4cde41c80aff81b7aaf2edd5a0
Mateo-Max <max.mateo.02@gmail.com>
2020-12-17 02:03:38 -0300

    json datos prueba terminado

--- public/app/datos.js
+++ public/app/datos.js
@@ -1,20 +1,29 @@
-function addDatosTest() {
-  fetch('../../src/assets/json/usersPrueba.json')
-      .then(response => response.json())
-      .then(data => {
-      // Do something with your data
-      const dataJSON = JSON.stringify(data)
-      localStorage.setItem('usersRegister',dataJSON)
+let dataJSON
+let dataJSON2
+
+fetch('../../src/assets/json/usersPrueba.json')
+  .then(response => response.json())
+  .then(data => {
+    // Do something with your data
+    dataJSON = JSON.stringify(data)
   });
-  fetch('../../src/assets/json/pelisPrueba.json')
-      .then(response => response.json())
-      .then(data => {
-      // Do something with your data
-      const dataJSON = JSON.stringify(data)
-      localStorage.setItem('userUI',dataJSON)
+fetch('../../src/assets/json/pelisPrueba.json')
+  .then(response => response.json())
+  .then(data => {
+    // Do something with your data
+    dataJSON2 = JSON.stringify(data)
   });
 
+function addDatosTest() {
+  localStorage.setItem('usersRegister',dataJSON)
+  localStorage.setItem('userUI',dataJSON2)
+  
+
+  
+  displayAllMoviesTable()
+  filtrarPeliculas()
+  displayAllUsersTable()
 }
-addDatosTest()
+
 
 
